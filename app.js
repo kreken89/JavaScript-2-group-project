a2dc6f22a310c3d5f8bae6abf02cfc0f777cddba
DVNO <dvno@live.se>
2023-01-12 19:29:48 +0100

    try to work

--- app.js
+++ app.js
@@ -14,29 +14,32 @@ const BASE_URL = 'https://fnd22-shared.azurewebsites.net/swagger/index.html';
 
 // Wrappa in GET i function
 // function showResult() {
-// }  
+// } 
 
-fetch('https://fnd22-shared.azurewebsites.net/api/Cases')
-  .then((res) => {
 
-      /* IF statement checks server response: .catch() does not do this! */
 
-    if (res.ok) {
-      console.log('HTTP request successful');
-    } else {
-      console.log('HTTP request unsuccessful');
-       }
-    return res;
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data)) // the data
-    .catch((error) => console.log(error)); // error handling
+// fetch('https://fnd22-shared.azurewebsites.net/api/Cases')
+//   .then((res) => {
+
+//       /* IF statement checks server response: .catch() does not do this! */
+
+//     if (res.ok) {
+//       console.log('HTTP request successful');
+//     } else {
+//       console.log('HTTP request unsuccessful');
+//        }
+//     return res;
+//     })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data)) // the data
+//     .catch((error) => console.log(error)); // error handling
 
   /* .catch handles a failure with fetch (e.g. syntax error, no internet connection) */
+ 
+
 
 
 // POST
-
 
 fetch('https://reqres.in/api/users', {
   method: 'POST', // // or "PUT" with the url changed to, e.g "https://reqres.in/api/users/2"
@@ -63,29 +66,50 @@ fetch('https://reqres.in/api/users', {
 
 // PUT
 
-  fetch('https://reqres.in/api/users/2', {
-    method: 'PUT', // // or "PUT" with the url changed to, e.g "https://reqres.in/api/users/2"
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      email: '',
-      subject: '',
-      message: ''
-    }),
-  })
-    .then((res) => {
-      if (res.ok) {
-        console.log('HTTP request successful');
-      } else {
-        console.log('HTTP request unsuccessful');
-      }
-      return res;
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+  // fetch('https://reqres.in/api/users/2', {
+  //   method: 'PUT', // // or "PUT" with the url changed to, e.g "https://reqres.in/api/users/2"
+  //   headers: {
+  //     'Content-type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     email: '',
+  //     subject: '',
+  //     message: ''
+  //   }),
+  // })
+  //   .then((res) => {
+  //     if (res.ok) {
+  //       console.log('HTTP request successful');
+  //     } else {
+  //       console.log('HTTP request unsuccessful');
+  //     }
+  //     return res;
+  //   })
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data))
+  //   .catch((error) => console.log(error));
 
 
 
 // DELETE
+
+// fetch('https://httpbin.org/anything', {
+//   method: 'DELETE',
+//   headers: {
+//     'Content-type': 'application/json',
+//   },
+// })
+//   .then((res) => {
+//     if (res.ok) {
+//       console.log('DELETE request successful');
+//       return res;
+//     } else {
+//       console.log('DELETE request unsuccessful');
+//     }
+//     return res;
+//   })
+//   .then((res) => res.json())
+//   .then((data) =>
+//     document.getElementById('output').append(JSON.stringify(data))
+//   )
+//   .catch((error) => console.log(error));
