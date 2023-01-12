3afc591289d4a598cce5ac6cd02315e9b69cc604
Gustav Clavin <gustav.clavin99@gmail.com>
2023-01-12 12:08:06 +0100

    Misslyckat försök att posta case.

--- app.js
+++ app.js
@@ -9,17 +9,48 @@ const message = document.querySelector('#message_input')
 const form = document.querySelector('#task_form')
 //const btn = document.querySelector(".btn")
 
-const api_copy = []
-
+const cases = []
+let JSONString = ''
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
 
     const tempObj = {email: email.value, subject: subject.value, message: message.value}
-    const jsonString = JSON.stringify(Object.assign(tempObj))
-    console.log(jsonString)
+    JSONString = JSON.stringify(Object.assign(tempObj))
+    
+    
 })
 
 
+/* const postCase = () => {
+    fetch("https://fnd22-shared.azurewebsites.net/api/Cases", {
+        method: 'POST',
+        body: JSONString
+        })
+        .then(res => res.json())
+        .then( data => console.log(data))
+        .catch(err =>  console.log(err))
+} */
+        
+
+     
 
 
+
+
+const getCase = () => {
+    
+    fetch("https://fnd22-shared.azurewebsites.net/api/Cases")
+        
+        .then(res => res.json())
+        .then(data => {
+            
+            data.forEach(element => {
+                cases.push(element)
+            });
+            console.log(cases)
+        })
+    
+}
+
+getCase()
