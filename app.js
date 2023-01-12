13a7a67138a90f62a1c3149156316c2f0ccaab38
Gustav Clavin <gustav.clavin99@gmail.com>
2023-01-12 11:01:38 +0100

    Converting user input to JSON object string

--- app.js
+++ app.js
@@ -2,3 +2,24 @@
 // https://fnd22-shared.azurewebsites.net/swagger/index.html
 
 const BASE_URL = 'https://fnd22-shared.azurewebsites.net/swagger/index.html';
+
+const email = document.querySelector('#email_input')
+const subject = document.querySelector('#subject_input')
+const message = document.querySelector('#message_input')
+const form = document.querySelector('#task_form')
+//const btn = document.querySelector(".btn")
+
+const api_copy = []
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const tempObj = {email: email.value, subject: subject.value, message: message.value}
+    const jsonString = JSON.stringify(Object.assign(tempObj))
+    console.log(jsonString)
+})
+
+
+
+
