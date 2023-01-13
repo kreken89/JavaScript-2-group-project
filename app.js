c47ed8d3abb04f789cae2f1d40b7b35df4be24dc
DVNO <dvno@live.se>
2023-01-13 10:28:33 +0100

    small changes and test

--- app.js
+++ app.js
@@ -1,42 +1,82 @@
-const BASE_URL = 'https://fnd22-shared.azurewebsites.net/swagger/index.html';
-const CASE_URL = 'https://fnd22-shared.azurewebsites.net/api/Cases';
-const email = document.querySelector('#email_input');
-const subject = document.querySelector('#subject_input');
-const message = document.querySelector('#message_input');
-const form = document.querySelector('#task_form');
+const BASE_URL = 'https://fnd22-shared.azurewebsites.net/swagger/index.html'
+const CASE_URL = 'https://fnd22-shared.azurewebsites.net/api/Cases'
+const email = document.querySelector('#email_input')
+const subject = document.querySelector('#subject_input')
+const message = document.querySelector('#message_input')
+const form = document.querySelector('#task_form')
+
+
+// Promises
+
+const getCases = function(url, errorMsg = 'Someting went wrong'){
+  return fetch(url).then(res => {
+    // Error msg
+    if(!res.ok) throw new Error (`${errorMsg} (${res.status})`)
+
+    return res.json()
+  })
+}
 
 // GET
 
-// function handleData(data){
-//   console.log(data);
-// }
+const getCaseData = function (){
+  getCases(
+    'https://fnd22-shared.azurewebsites.net/api/Cases',
+    'Someting went wrong'
+  )
+    .then((res) => {
+      /* IF statement checks server response: .catch() does not do this! */
 
-// Wrappa in GET i function
+      if (res.ok) {
+        console.log('HTTP request successful');
+      } else {
+        console.log('HTTP request unsuccessful');
+      }
+      return res;
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data)) // the data
+    .catch((error) => console.log(error)); // error handling
+
+  }
+
+
+// Wrap in GET i function
 // function showResult() {
 // } 
 
 
 
-// fetch('https://fnd22-shared.azurewebsites.net/api/Cases')
-//   .then((res) => {
+fetch('https://fnd22-shared.azurewebsites.net/api/Cases')
+  .then((res) => {
 
-//       /* IF statement checks server response: .catch() does not do this! */
+      /* IF statement checks server response: .catch() does not do this! */
 
-//     if (res.ok) {
-//       console.log('HTTP request successful');
-//     } else {
-//       console.log('HTTP request unsuccessful');
-//        }
-//     return res;
-//     })
-//     .then((res) => res.json())
-//     .then((data) => console.log(data)) // the data
-//     .catch((error) => console.log(error)); // error handling
+    if (res.ok) {
+      console.log('HTTP request successful');
+    } else {
+      console.log('HTTP request unsuccessful');
+       }
+    return res;
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data)) // the data
+    .catch((error) => console.log(error)); // error handling
 
   /* .catch handles a failure with fetch (e.g. syntax error, no internet connection) */
  
+const cases = []
+const dataName = {
+  email: '',
+  subject: '',
+  message: ''
+}
 
-
+array.sort(function (a, b) {
+  // Turn your strings into dates, and then subtract them
+  // to get a value that is either negative, positive, or zero.
+  return new Date(b.date) - new Date(a.date);
+});
 
 // POST
 
@@ -53,15 +93,16 @@ fetch('https://reqres.in/api/users', {
 })
   .then((res) => {
     if (res.ok) {
-      console.log('HTTP request successful');
+      console.log('HTTP request successful')
     } else {
-      console.log('HTTP request unsuccessful');
+      console.log('HTTP request unsuccessful')
     }
     return res;
   })
   .then((res) => res.json())
   .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .then(console.log(dataName))
+  .catch((error) => console.log(error))
 
 // PUT
 
