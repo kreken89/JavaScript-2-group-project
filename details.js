95ba4b50fd4f403992bf4105fe9f79bfc2ffa8a6
Gustav Clavin <gustav.clavin99@gmail.com>
2023-01-17 12:25:39 +0100

    details updated

--- details.js
+++ details.js
@@ -1,6 +1,6 @@
 const id = new URLSearchParams(window.location.search).get('id')
 const CASE_URL = 'https://fnd22-shared.azurewebsites.net/api/Cases/';
-const wrapper = document.querySelector('.wrapper');
+const wrapper = document.querySelector('.container_details');
 
 console.log(id)
 console.log(CASE_URL + id)
@@ -12,21 +12,46 @@ const getCase = () => {
         console.log(data)
 
         wrapper.innerHTML =
-        `<div id="output" class="container_details">
-            <div class="card bg-secondary p-2 text-white">
-            <h2>${data.subject}</h2>
-            <p>${data.message}</p>
-            
-        </div>
-        
         `
+            <div class="inline">
+                <ul>
+                    <li class="green status">Avslutad</li>
+                    <li class="orange status">Pågående</li>
+                    <li class="red status">Ej påbörjad</li>
+                </ul>
+                <span class="time_add">${data.created.replace('T', ' ').substring(0, 16)}</span>
+            </div>
+            <div class="card bg-secondary p-2 text-white">
+                <h2>${data.subject}</h2>
+                <p>${data.email}</p>
+                <p>${data.message}</p>
+                <br>
+                <h2>Comments:</h2>
+                
+            </div>
+            <div class="userInput">
+            <textarea name="message" id="message_input" placeholder="New comment..."></textarea>
+            <input type="submit" value="Add Comment" class="btn">
+            `
+
+            const commentList = document.createElement('ul')
+            commentList.className = 'commentList'
+            wrapper.appendChild(commentList)
+            console.log(data.comments.length)
+            for(let i = 0; i<data.comments.length; i++){
+                const comment = document.createElement('li')
+                comment.className = 'comment'
+                comment.innerText = `${data.comments[i]}`
+                commentList.appendChild(comment)
+            }
 
       });
   };
+
+  const postComment = () => {
+    
+  }
+
   getCase()
 
-/* <div id="output" class="container_details">
-    <div class="card bg-secondary p-2 text-white">
-    <h2>Case details</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, similique....</p>
-</div> */
+  
