383342b8474fee945cd5804d7318550fe7f2b290
Gustav Clavin <gustav.clavin99@gmail.com>
2023-01-18 15:45:45 +0100

    commit igen?

--- details.js
+++ details.js
@@ -102,7 +102,7 @@ const getCase = () => {
       .then((data) => {
         console.log(data)
       })
-      /* .catch((err) => console.log(err)); */
+      .catch((err) => console.log(err));
   }
 
   const putStatus = () => {
