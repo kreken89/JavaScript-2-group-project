f19c80030811a9f59e9a12eb8e4286addf37c6e2
Gustav Clavin <gustav.clavin99@gmail.com>
2023-01-18 11:29:29 +0100

    tagit bort innerHTML i details

--- details.js
+++ details.js
@@ -3,6 +3,7 @@ const CASE_URL = 'https://fnd22-shared.azurewebsites.net/api/Cases/';
 const COMMENT_URL = 'https://fnd22-shared.azurewebsites.net/api/Comments/';
 const wrapper = document.querySelector('.container_details');
 const form = document.querySelector('.userInput')
+const inline = document.querySelector('.inline')
 
 
 let newComment = {}
@@ -27,7 +28,50 @@ const getCase = () => {
       .then((data) => {
         console.log(data)
         email = data.email
-        wrapper.innerHTML =
+        /*  */
+        /* const inline = document.createElement('div')
+        inline.className = 'inline'
+        wrapper.appendChild(inline) */
+        const statusInfo = document.createElement('div')
+        statusInfo.className = 'statusInfo'
+        inline.appendChild(statusInfo)
+        /* const radio1 = document.createElement('radio')
+        const radio2 = document.createElement('radio')
+        const radio3 = document.createElement('radio')
+        radio1.className = 'red'
+        radio2.className = 'orange'
+        radio3.className = 'green'
+        radio1.id = '1'
+        radio2.id = '2'
+        radio3.id = '3'
+        radio1.innerText = 'Ej påbörjad'
+        radio2.innerText = 'Pågående'
+        radio3.innerText = 'Avslutad'
+        statusInfo.appendChild(radio1)
+        statusInfo.appendChild(radio2)
+        statusInfo.appendChild(radio3) */
+        const time_add = document.createElement('span')
+        time_add.className = 'time_add'
+        time_add.innerText = data.created.replace('T', ' ').substring(0, 16)
+        wrapper.appendChild(time_add)
+        const card = document.createElement('div')
+        card.className = 'card'
+        wrapper.appendChild(card)
+        const subject = document.createElement('h2')
+        const _email = document.createElement('p')
+        const message = document.createElement('p')
+        subject.innerText = data.subject
+        _email.className = 'p_details'
+        message.className = 'p_details'
+        _email.innerText = data.email
+        message.innerText = data.message
+        card.appendChild(subject)
+        card.appendChild(_email)
+        card.appendChild(message)
+        const commentsHeadline = document.createElement('h2')
+        commentsHeadline.innerText = 'Comments:'
+        wrapper.appendChild(commentsHeadline)
+        /* wrapper.innerHTML =
         `
             <div class="inline">
                 <div class="statusInfo">
@@ -49,7 +93,7 @@ const getCase = () => {
             </div>
             <h2>Comments:</h2>
             
-            ` + wrapper.innerHTML
+            ` + wrapper.innerHTML */
 
             const commentList = document.createElement('ul')
             commentList.className = 'commentList'
